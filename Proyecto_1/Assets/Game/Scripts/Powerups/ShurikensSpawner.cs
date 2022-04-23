13fadae7a7fae5b57d1f860b1988a8bc0fc61012
Carlos Marcano <cmarca@gmail.com>
2022-04-23 15:53:25 -0400

    Shuriken respawn

--- Proyecto_1/Assets/Game/Scripts/Powerups/ShurikensSpawner.cs
+++ Proyecto_1/Assets/Game/Scripts/Powerups/ShurikensSpawner.cs
@@ -3,30 +3,101 @@ using UnityEngine;
 
 public class ShurikensSpawner : MonoBehaviour
 {
+
+    int numberOfObjects = 5;
     [SerializeField] float spawnRate;
     Transform player;
     [SerializeField] GameObject shurikenPrefab;
+    [SerializeField] GameObject[] shurikenPrefabs;
     public int shurikenAmount = 0;
     public bool canSpawnShuriken;
+    System.Threading.Timer timer;
+
+    float timeCounter = 0;
+    bool needRespawn = false;
+	float needToRespawnTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+    	shurikenPrefabs = new GameObject[numberOfObjects];
         player = FindObjectOfType<PlayerController>().transform;
-        StartCoroutine(ShurikenSpawner());
+        // StartCoroutine(ShurikenSpawner());
 
+		crearAllShuriken();
+		// crearShurikenAfter(5000);
     }
 
-    IEnumerator ShurikenSpawner()
-    {
-        while (true)
-        {
-            {
-                yield return new WaitForSeconds(spawnRate);
-                Instantiate(shurikenPrefab, player.position, Quaternion.identity);
-                shurikenAmount++;
-            }
+  //   void activatedAllShurikensAfter(int miliseconds) {
+  //   	Debug.Log("testing Timer before");
+		// timer = new System.Threading.Timer((obj) => {
+  //       	canSpawn = true;
+	 //        crearAllShuriken();
+  //       	timer.Dispose();
+	 //    }, null, miliseconds, 1000);
+  //   }
 
-        }
+    void crearAllShuriken() {
+    	int radius = 3;
+    	for (int i = 0; i < numberOfObjects; i++)  {
+	        Debug.Log("testing Timer");
+
+			float angle = i * Mathf.PI * 2 / numberOfObjects;
+			float x = Mathf.Cos(angle) * radius;
+			float z = Mathf.Sin(angle) * radius;
+			Vector3 pos = new Vector3(x, z, 0);
+
+	        GameObject shuriken = Instantiate(shurikenPrefab, pos, Quaternion.identity);  		
+    		shurikenPrefabs[i] = shuriken;
+    	}
     }
+
+    void Update() {
+		Transform player = FindObjectOfType<PlayerController>().transform;
+		int radius = 3;
+		timeCounter += Time.deltaTime;
+
+		var allNotActive = true;
+
+		for (int i = 0; i < numberOfObjects; i++)  {
+			float angle = i * Mathf.PI * 2 / numberOfObjects;
+			float x = Mathf.Cos(angle + timeCounter) * radius;
+			float z = Mathf.Sin(angle + timeCounter) * radius;
+			Vector3 pos = new Vector3(x, z, 0);
+
+			shurikenPrefabs[i].transform.position = pos + player.position;
+
+			allNotActive = allNotActive && !shurikenPrefabs[i].activeSelf;
+		}
+
+		if (allNotActive && needToRespawnTime == 0) {
+			needToRespawnTime = Time.fixedTime;
+			needRespawn = true;
+		}
+
+		Debug.Log(needRespawn);
+		Debug.Log(Time.fixedTime - needToRespawnTime);
+			
+		if (needRespawn && (Time.fixedTime - needToRespawnTime > 5.0)) {
+			for (int i = 0; i < numberOfObjects; i++)  {
+				 shurikenPrefabs[i].GetComponent<ShurikenController1>().shurikenHealth = 5;
+				 shurikenPrefabs[i].SetActive(true);
+			}
+			needRespawn = false;
+			needToRespawnTime = 0;
+		}
+    }
+
+    // IEnumerator ShurikenSpawner()
+    // {
+    //     while (true)
+    //     {
+    //         {
+    //             // yield return new WaitForSeconds(spawnRate);
+    //             // Instantiate(shurikenPrefab, player.position, Quaternion.identity);
+    //             // shurikenAmount++;
+    //         }
+
+    //     }
+    // }
 }
--- Proyecto_1/Assets/Game/Scripts/ShurikenController1.cs
+++ Proyecto_1/Assets/Game/Scripts/ShurikenController1.cs
@@ -9,25 +9,33 @@ public class ShurikenController1 : MonoBehaviour
     [SerializeField] int angularSpeed;
     [SerializeField] float angles;
     [SerializeField] int damage;
-    [SerializeField] int shurikenHealth = 5;
+    public int shurikenHealth = 5;
     Vector3 vectorPosition;
     float timeCounter = 0;
     // Start is called before the first frame update
+
+    // Vector3 originalPosition;
+
     void Start()
     {
         player = FindObjectOfType<PlayerController>().transform;
+        // originalPosition = transform.position;
+        // timeCounter += Time.deltaTime * angularSpeed;
+        // vectorPosition = new Vector3(Mathf.Cos(timeCounter) * radius, Mathf.Sin(timeCounter) * radius, 0);
+        // transform.position = player.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeCounter += Time.deltaTime * angularSpeed;
-        vectorPosition = new Vector3(Mathf.Cos(timeCounter) * radius, Mathf.Sin(timeCounter) * radius, 0);
-        transform.position = vectorPosition + player.position;
+        // timeCounter += Time.deltaTime * angularSpeed;
+        // vectorPosition = new Vector3(Mathf.Cos(timeCounter) * radius, Mathf.Sin(timeCounter) * radius, 0);
+        // transform.position = originalPosition + player.position;
 
         if(shurikenHealth <= 0)
         {
-            Destroy(gameObject);
+            // Destroy(gameObject);
+            gameObject.SetActive(false);
             FindObjectOfType<ShurikensSpawner>().shurikenAmount--;
         }
     }
