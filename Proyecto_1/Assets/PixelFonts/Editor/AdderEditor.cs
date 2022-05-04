806ac3315f6cc0caf3e10e1357b9cc23209773e5
dfmr96 <dfmr96@gmail.com>
2022-05-04 00:08:59 -0300

    GUI improvement

---
+++ Proyecto_1/Assets/PixelFonts/Editor/AdderEditor.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+public class AdderEditor : EditorWindow
+{
+	public Font Font;
+	public Texture2D Texture;
+	public Material Material;
+	public AnimatorController Parent;
+	public AnimationClip Child;
+	[MenuItem("Deko/Adder")]
+	static public void Init()
+	{
+		EditorWindow.GetWindow(typeof(AdderEditor)).Show();
+	}
+	void OnGUI()
+	{
+		Font = EditorGUILayout.ObjectField(Font, typeof(Font), false) as Font;
+		Texture = EditorGUILayout.ObjectField(Texture, typeof(Texture2D), false) as Texture2D;
+		Material = EditorGUILayout.ObjectField(Material, typeof(Material), false) as Material;
+		if (GUILayout.Button("Add Font"))
+		{
+			AddFont();
+		}
+		if (GUILayout.Button("Extract Font Texture"))
+		{
+			ExtractFontTexture();
+		}
+		Parent = EditorGUILayout.ObjectField(Parent, typeof(AnimatorController), false) as AnimatorController;
+		Child = EditorGUILayout.ObjectField(Child, typeof(AnimationClip), false) as AnimationClip;
+		if (GUILayout.Button("Add Animation"))
+		{
+			AddAnimations();
+		}
+		if (GUILayout.Button("Add Selected Animations"))
+		{
+			AddSelectedAnimations();
+		}
+	}
+	void AddFont()
+	{
+		var name = Path.GetFileNameWithoutExtension(AssetDatabase.GetAssetPath(Font));
+		var newTexture = Object.Instantiate(Texture) as Texture2D;
+		newTexture.name = name;
+		var newMaterial = Object.Instantiate(Material) as Material;
+		newMaterial.name = name;
+		newMaterial.mainTexture = newTexture;
+		Font.material = newMaterial;
+		AssetDatabase.AddObjectToAsset(newTexture, Font);
+		AssetDatabase.AddObjectToAsset(newMaterial, Font);
+		AssetDatabase.SaveAssets();
+	}
+	void ExtractFontTexture()
+	{
+		var bytes = Texture.EncodeToPNG();
+		File.WriteAllBytes(EditorUtility.SaveFilePanel("Save PNG", Application.dataPath + "/../", "Font", "png"), bytes);
+	}
+	void AddAnimations()
+	{
+		var newAnimation = Object.Instantiate(Child) as AnimationClip;
+		newAnimation.name = Child.name;
+		AssetDatabase.AddObjectToAsset(newAnimation, Parent);
+		AssetDatabase.SaveAssets();
+	}
+	void AddSelectedAnimations()
+	{
+		var clips = Selection.GetFiltered(typeof(AnimationClip), SelectionMode.Unfiltered);
+		if (clips != null && clips.Length > 0)
+		{
+			foreach (var clip in clips)
+			{
+				var newAnimation = Object.Instantiate(clip) as AnimationClip;
+				newAnimation.name = clip.name;
+				AssetDatabase.AddObjectToAsset(newAnimation, Parent);
+			}
+			AssetDatabase.SaveAssets();
+			Selection.objects = new Object[0];
+		}
+	}
+}
---
+++ Proyecto_1/Assets/PixelFonts/ReadMe.txt
@@ -0,0 +1,15 @@
+PixelFonts
+art: benmhenry@gmail.com
+code: davidahenry@gmail.com
+
+Description:
+Three pixel perfect bitmap fonts. SuperWhite, SuperBlack and Venice.
+
+Documentation:
+Files:
+PixelFonts/SuperBlack.fontsettings : SuperBlack font
+PixelFonts/SuperWhite.fontsettings : SuperWhite font
+PixelFonts/Venice.fontsettings : Venice font
+PixelFonts/Fonts.unity : example scene using UnityEngine.UI.Text
+PixelFonts/FontsMesh.unity : example scene using UnityEngine.TextMesh
+PixelFonts/ReadMe.txt : this
