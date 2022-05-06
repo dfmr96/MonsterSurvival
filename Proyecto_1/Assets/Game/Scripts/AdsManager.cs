e338e5784bd6cecf6c4d1958ed050160191a0e68
dfmr96 <dfmr96@gmail.com>
2022-05-06 14:49:06 -0300

    Revive buttom + ads to revive once added;

---
+++ Proyecto_1/Assets/Game/Scripts/AdsManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+ 
+public class AdsManager : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId = "4742537";
+    [SerializeField] string _iOSGameId = "4742536";
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+
+    void Awake()
+    {
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOSGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, this);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+    }
+}
